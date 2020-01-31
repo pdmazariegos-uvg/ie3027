acc4544dc50f1c203e86b0b5191e214b24e9f12a
Pablo Mazariegos <pdmazariegos-uvg@users.noreply.github.com>
2020-01-31 11:23:42 -0600

    Plantilla Ejemplo LCD 4 bits
    Se inició el código de la plantilla ejemplo del programa para utilizar la LCD 16x2 en formato de 4 Bits

---
+++ Ejemplos/LCD4Bits.X/main.c
@@ -0,0 +1,35 @@
+/*
+ * File:   main.c
+ * Author: Pablo
+ * Ejemplo de uso de la LCD 16x2 en modo 4 bits
+ * Se utilizó y se adaptaron las librerías de Ligo George 
+ * de la página www.electrosome.com
+ * Enlace: https://electrosome.com/lcd-pic-mplab-xc8/
+ * Created on 31 de enero de 2020, 11:20 AM
+ */
+
+#pragma config FOSC = EXTRC_NOCLKOUT// Oscillator Selection bits (RCIO oscillator: I/O function on RA6/OSC2/CLKOUT pin, RC on RA7/OSC1/CLKIN)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled and can be enabled by SWDTEN bit of the WDTCON register)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config MCLRE = OFF      // RE3/MCLR pin function select bit (RE3/MCLR pin function is digital input, MCLR internally tied to VDD)
+#pragma config CP = OFF         // Code Protection bit (Program memory code protection is disabled)
+#pragma config CPD = OFF        // Data Code Protection bit (Data memory code protection is disabled)
+#pragma config BOREN = OFF      // Brown Out Reset Selection bits (BOR disabled)
+#pragma config IESO = OFF       // Internal External Switchover bit (Internal/External Switchover mode is disabled)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is disabled)
+#pragma config LVP = OFF        // Low Voltage Programming Enable bit (RB3 pin has digital I/O, HV on MCLR must be used for programming)
+
+// CONFIG2
+#pragma config BOR4V = BOR40V   // Brown-out Reset Selection bit (Brown-out Reset set to 4.0V)
+#pragma config WRT = OFF        // Flash Program Memory Self Write Enable bits (Write protection off)
+
+#include <xc.h>
+#include <stdint.h>
+
+void main(void) {
+    
+    while(1){
+    
+    }
+    return;
+}
