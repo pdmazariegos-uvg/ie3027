ceea059b9720b5f4aeb90ebe2afee0d011c580bb
Pablo Mazariegos <pdmazariegos-uvg@users.noreply.github.com>
2020-01-31 11:29:04 -0600

    Se agregó los prototipos de las funciones
    Se agregó al archivo header los prototipos de las funciones de la librería de LCD

--- Ejemplos/LCD4Bits.X/LCD.h
+++ Ejemplos/LCD4Bits.X/LCD.h
@@ -20,9 +20,10 @@
  */
 
 /* 
- * File:   
- * Author: 
- * Comments:
+ * File: LCD.h  
+ * Se utilizó y se adaptaron las librerías de Ligo George 
+ * de la página www.electrosome.com
+ * Enlace: https://electrosome.com/lcd-pic-mplab-xc8/
  * Revision history: 
  */
 
@@ -33,7 +34,26 @@
 
 #include <xc.h> // include processor files - each processor file is guarded.  
 
+//LCD Functions Developed by electroSome
 
+
+void Lcd_Port(char a);
+
+void Lcd_Cmd(char a);
+
+void Lcd_Clear(void);
+
+void Lcd_Set_Cursor(char a, char b);
+
+void Lcd_Init(void);
+
+void Lcd_Write_Char(char a);
+
+void Lcd_Write_String(char *a);
+
+void Lcd_Shift_Right(void);
+
+void Lcd_Shift_Left(void);
 
 #endif	/* XC_HEADER_TEMPLATE_H */
 
